5d39c2b2c9a77921944d375ef61c0e22af7f919c
Jesus-Acevedo-Cano <jeacevedocano@gmail.com>
2021-09-17 02:56:16 -0500

    shopping cart fixed

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,19 @@
 import "./Header.css"
 import logo from './assets/images/saatva_logo.png'
 import cart from './assets/images/shopping-cart-1985.png'
+import { service } from './matressesServices'
+import { useState } from "react";
+import { take } from "rxjs";
 
-export const shoppingCart = [];
 function Header() {
-    const shoppingCartLength = shoppingCart.length
+    const [shoppingCart, setValue] = useState(0)
+    service.getData().pipe(take(1)).subscribe(res => {setValue(res.length); console.log(res)})
     return (
         <header className="App-header">
             <a href="/" title="Navigate to home page"><img src={logo} alt="Saatva logo" height='30px'></img></a>
             <a className="shop-cart" href="/" title="Navigate to shopping cart">
                 <img src={cart} alt="shopping cart icon"></img>
-                <span>{shoppingCartLength}</span>
+                <span>{shoppingCart}</span>
             </a>
       </header>
     )
--- src/Matresses.js
+++ src/Matresses.js
@@ -1,14 +1,15 @@
 import './Mattresses.css'
 import React, { useState } from 'react'
 import data from './assets/mattresses.json'
-import { shoppingCart } from './Header'
+import { service } from './matressesServices'
 import classic_img from './assets/images/classic-carousel.jpg'
 import loom_img from './assets/images/loom-carousel.jpg'
 import zen_img from './assets/images/zen-carousel.jpg'
 
+const shoppingCart = [];
 function Mattresses() {
-  const [value, setValue] = useState("classic")
-  const mattresses = data.mattresses
+  const [value, setValue] = useState("classic");
+  const mattresses = data.mattresses;
   let source = classic_img;
   if (value === "loom") source = loom_img;
   else if (value === "zen") source = zen_img;
@@ -38,7 +39,7 @@ function Mattresses() {
         {/* button to add item to cart depends on his value acting as a key */}
         <button className="add_btn" onClick={() => {
           shoppingCart.push(mattresses[value]);
-          console.log(shoppingCart)
+          service.setData(shoppingCart)
         }}>Add to Cart</button>
       </div>
     </div>
